248e603b3068c867d1bb87b1a3706f4c4cbbcb09
afek <afek@spotlightltd.com>
2019-12-03 19:44:23 +0200

    updated version. als works

---
+++ HW1/optimization_objects.py
@@ -0,0 +1,24 @@
+class LearningRateScheduler:
+    def __init__(self, lr=0.01, decay=0.9):
+        self.lr = lr
+        self.decay = decay
+
+    def update(self, n):
+        if n > 1:
+            self.lr *= self.decay
+        return self.lr
+
+
+class EarlyStopping:
+    def __init__(self, n_iter, min_epoch=15):
+        self.n_iter = n_iter
+        self.last_value = None
+        self.consecutive_increasing_errors = 0
+        self.min_epoch = min_epoch
+
+    def stop(self, epoch, error):
+        if error > self.last_value and epoch >= self.min_epoch:
+            self.consecutive_increasing_errors += 1
+        if self.consecutive_increasing_errors >= self.n_iter:
+            return True
+        return False
