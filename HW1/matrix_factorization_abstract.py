5f0dfeb2fdd9d9e61c28865b0c1c6c654ae952d5
wolkomor <wolkomor@gmail.com>
2019-12-05 14:36:47 +0200

    add r2+mse

--- HW1/matrix_factorization_abstract.py
+++ HW1/matrix_factorization_abstract.py
@@ -1,7 +1,6 @@
 import numpy as np
 import pandas as pd
-from sklearn.metrics import r2_score
-from numpy import linalg as LA
+
 
 class MatrixFactorizationWithBiases:
     def __init__(self, seed, hidden_dimension):
@@ -21,8 +20,7 @@ class MatrixFactorizationWithBiases:
             self.results[key].append(value)
             print(f"{key} : {np.round(value, 5)}")
 
-
-    def fit(self, train: np.array, validation: np.array, user_map: dict, item_map: dict):
+    def fit(self, train: pd.DataFrame, validation: pd.DataFrame, user_map: dict, item_map: dict):
         pass
 
     def predict(self, user, item):
@@ -43,31 +41,31 @@ class MatrixFactorizationWithBiases:
                 prediction = self.predict_on_new_user_new_item()
         return np.clip(prediction, 1, 5)
 
-    def predict_on_pair(self, user, item):
-
-        # TODO make sure that if we see a new user we return the global mean and if we have a new item and an
-        #  existing user exc exc
-        return self.global_bias + self.user_biases[user] + self.item_biases[item] \
-               + self.U[user, :].dot(self.V[item, :].T)
-
-    def predict_on_new_user_existing_item(self, item):
-        return self.global_bias + self.item_biases[item]
-
-    def predict_on_existing_user_new_item(self, user):
-        return self.global_bias + self.user_biases[user]
-
-    def predict_on_new_user_new_item(self):
-        return self.global_bias
-
     def calc_loss(self, x):
         loss = 0
-        parameters_biases = [(self.user_biases, self.l2_users_bias), (self.item_biases,self.l2_items_bias)]
-        parameters = [(self.U, self.l2_users), (self.V, self.l2_items)]
-        for b in parameters_biases:
-            loss += b[1] * np.sum(np.square(b[0]))
-        for p in parameters:
-            loss += p[1] * np.sum(LA.norm(p[0], axis=1))
-        return loss + self.prediction_loss(x)
+        parameters = [self.user_biases, self.item_biases, self.U, self.V]
+        regularizations = [self.l2_users_bias, self.l2_items_bias, self.l2_users, self.l2_items]
+        for i in range(len(parameters)):
+            loss += regularizations[i] * np.sum(np.square(parameters[i]))
+        return loss + self.prediction_error(x)
+
+    def r2(self, x):
+        denominator = 0
+        nominator = 0
+        for row in x:
+            user, item, rating = row
+            denominator += np.square(self.predict_on_pair(user, item)-self.global_bias)
+            nominator += np.square(rating - self.predict_on_pair(user, item))
+        return float(nominator)/denominator
+
+
+    def mse(self, x):
+        e = 0
+        for row in x:
+            user, item, rating = row
+            e += np.square(rating - self.predict_on_pair(user, item))
+        return e / x.shape[0]
+
 
     def rmse(self, x):
         e = 0
@@ -81,12 +79,23 @@ class MatrixFactorizationWithBiases:
         for row in x:
             user, item, rating = row
             e += np.abs(rating - self.predict_on_pair(user, item))
-        return e/x.shape[0]
+        return e / x.shape[0]
 
-    def prediction_loss(self, x, measure_function="rmse"):
-        if measure_function=="rmse":
+    def prediction_error(self, x, measure_function="rmse"):
+        if measure_function == "rmse":
             return self.rmse(x)
-        elif measure_function=="mae":
+        elif measure_function == "mae":
             return self.mae(x)
 
+    def predict_on_pair(self, user, item):
+        return self.global_bias + self.user_biases[user] + self.item_biases[item] \
+               + self.U[user, :].dot(self.V[item, :].T)
 
+    def predict_on_new_user_existing_item(self, item):
+        return self.global_bias + self.item_biases[item]
+
+    def predict_on_existing_user_new_item(self, user):
+        return self.global_bias + self.user_biases[user]
+
+    def predict_on_new_user_new_item(self):
+        return self.global_bias
