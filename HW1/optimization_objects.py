22df9e553b2dec827ca09f33dbd0a1538aa6d95a
afek <afek@spotlightltd.com>
2019-12-07 17:23:49 +0200

    added config

--- HW1/optimization_objects.py
+++ HW1/optimization_objects.py
@@ -36,3 +36,13 @@ class EarlyStopping:
         return False
 
 
+class Config:
+    def __init__(self, **kwargs):
+        self._set_attributes(kwargs)
+
+    def _set_attributes(self, kwargs):
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+    def add_attributes(self, **kwargs):
+        self._set_attributes(kwargs)
