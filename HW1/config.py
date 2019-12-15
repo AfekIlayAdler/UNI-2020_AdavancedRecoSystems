36a3d8ea3726e6b16cca4e241518103339746c68
afek <afek@spotlightltd.com>
2019-12-15 19:42:42 +0200

    fixed pycharm comment

--- HW1/config.py
+++ HW1/config.py
@@ -47,7 +47,7 @@ ALS_CONFIG = Config(
     bias_epochs=2,
     seed=SEED)
 
-#best starting point
+# best starting point
 if SGD:
     x0 = [[0.025, 0.01, 0.01, 0.001, 0.001, 18]]
     y0 = [0.8945]
