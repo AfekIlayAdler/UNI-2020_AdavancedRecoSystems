69f709f1a74cb95e7e4e2b50c2480391c96ae2a4
wolkomor <wolkomor@gmail.com>
2019-12-20 13:57:24 +0200

    update last run adjustments

--- HW1/main.py
+++ HW1/main.py
@@ -9,7 +9,7 @@ from skopt.plots import plot_convergence
 from HW1.MatrixFactorizationModelSGD import MatrixFactorizationWithBiasesSGD
 from HW1.MartixFactorizationModelALS import MatrixFactorizationWithBiasesALS
 from HW1.config import TRAIN_PATH, VALIDATION_PATH, HYPER_PARAM_SEARCH, ALS_SPACE, SGD, SGD_SPACE, SGD_CONFIG, \
-    ALS_CONFIG, HYPER_PARAM_SEARCH_N_ITER, SEED, CHECKPOINT_NAME, HYPER_PARAM_FILE_NAME, x0, y0
+    ALS_CONFIG, HYPER_PARAM_SEARCH_N_ITER, SEED, CHECKPOINT_NAME, HYPER_PARAM_FILE_NAME, x0, y0, LAST_RUN
 from HW1.utils import preprocess_for_mf
 
 space = SGD_SPACE if SGD else ALS_SPACE
@@ -30,7 +30,7 @@ def objective(**params):
     return mf.fit(train, validation, user_map, item_map)
 
 
-def run_exp(model,train, user_map, item_map, validation=None, last_run=False):
+def run_exp(model, train, user_map, item_map, validation=None, last_run=False):
     if HYPER_PARAM_SEARCH:
         #checkpoint_saver = CheckpointSaver(CHECKPOINT_NAME)
         #callback=[checkpoint_saver]
@@ -49,15 +49,16 @@ def run_exp(model,train, user_map, item_map, validation=None, last_run=False):
 
 if __name__ == '__main__':
     train, validation = pd.read_csv(TRAIN_PATH), pd.read_csv(VALIDATION_PATH)
-    train_all = pd.concat([train, validation], ignore_index=True)
-    train, validation, user_map, item_map = preprocess_for_mf(train, validation)
-    n_users, n_items = len(user_map), len(item_map)
-    mf = get_mf(n_users, n_items)
-    trained_model = run_exp(mf, train, user_map, item_map, validation)
 
-    # Final Run on all of the train data
-    train_all, user_map, item_map = preprocess_for_mf(train_all)
-    n_users, n_items = len(user_map), len(item_map)
-    trained_model.n_users = n_users
-    trained_model.n_items = n_items
-    FinalModel = run_exp(trained_model, train, user_map, item_map, validation=None, last_run=True)
+    if LAST_RUN:
+        # Final Run on all of the train data
+        train_all = pd.concat([train, validation], ignore_index=True)
+        train_all, user_map, item_map = preprocess_for_mf(train_all)
+        n_users, n_items = len(user_map), len(item_map)
+        mf = get_mf(n_users, n_items)
+        FinalModel = run_exp(mf, train_all, user_map, item_map, validation=None, last_run=True)
+    else:
+        train, validation, user_map, item_map = preprocess_for_mf(train, validation)
+        n_users, n_items = len(user_map), len(item_map)
+        mf = get_mf(n_users, n_items)
+        trained_model = run_exp(mf, train, user_map, item_map, validation)
