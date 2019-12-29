3fcfc162a33aed6e8d8b3a68eaa0aebdea61b206
afek <afekilayadler@gmail.com>
2019-12-29 16:33:34 +0200

    added bpr. added n_users and n_items

--- HW2/main.py
+++ HW2/main.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-from HW2.config import SEED, ONE_CLASS_MF_WEIGHT_DIR
+from HW2.config import SEED, ONE_CLASS_MF_WEIGHT_DIR, BPR, RESULT_DIR, ONE_CLASS_MF_RESULT_FILE_NAME
 from HW2.config import TRAIN_PATH
 from HW2.optimization_objects import Config
 from HW2.utils import preprocess_for_mf, create_directories
@@ -17,16 +17,21 @@ CONFIG = Config(
     l2_items=0.01,
     l2_users_bias=0.001,
     l2_items_bias=0.001,
-    epochs=25,
-    bias_epochs=5,
+    epochs=10,
+    bias_epochs=2,
     seed=SEED)
 
 if __name__ == "__main__":
-    train = pd.read_csv(TRAIN_PATH, nrows=5000)
-    create_directories([ONE_CLASS_MF_WEIGHT_DIR])
+    train = pd.read_csv(TRAIN_PATH)
+    create_directories([ONE_CLASS_MF_WEIGHT_DIR, RESULT_DIR])
     train, user_map, item_map = preprocess_for_mf(train)
-    mf = OneClassMatrixFactorizationWithBiasesSGD(CONFIG)
-    negative_sampler = NegativeSampler()
     validation_creator = ValidationCreator()
     train, validation = validation_creator.get(train)
-    mf.fit(train, user_map, item_map, negative_sampler, validation)
+    config = CONFIG
+    config.add_attributes(n_users=len(user_map), n_items=len(item_map))
+    if BPR:
+        pass
+    else:
+        mf = OneClassMatrixFactorizationWithBiasesSGD(config, NegativeSampler())
+        mf.fit(train, user_map, item_map, validation)
+        mf.get_results().to_csv(RESULT_DIR / ONE_CLASS_MF_RESULT_FILE_NAME, index = False)
